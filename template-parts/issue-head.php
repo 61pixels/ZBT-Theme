9723167505ec1d4fdfc0037008c7ac55523924ee
Kevin Hoffman <kevin@kevinwhoffman.com>
2016-08-05 15:55:37 -0400

    Reflect term slugs in anchor tags.

--- template-parts/issue-head.php
+++ template-parts/issue-head.php
@@ -4,21 +4,21 @@
 			<div class="twelve columns">
 				<ul class="inbl"> <!-- these will be anchors down to sections on the page -->
 					<li><a href="#features">Features</a></li>
-					<li><a href="#chapter">Alumni &amp; Chapter News</a></li>
+					<li><a href="#alumni-chapter-news">Alumni &amp; Chapter News</a></li>
 					<li><a href="#foundation">Foundation</a></li>
 					<li><a href="#legacy">Legacies</a></li>
-					<li><a href="#eternal">Chapter Eternal</a></li>
-					<li><a href="#volunteer">Volunteers</a></li>
+					<li><a href="#chapter-eternal">Chapter Eternal</a></li>
+					<li><a href="#volunteers">Volunteers</a></li>
 					<li><a href="#more">More</a></li>
 				</ul>
-				<ul>				
+				<ul>
 			</div>
 		</div>
 	</section>
 <?php if(is_front_page()) { ?>
 	<section class="featured-hero">
 		<div class="flexslider">
-			<!-- 
+			<!--
 			obviously harcoded for now with some sample slides, need query here to output
 			Query in this homepage case needs to pull all posts from the MOST RECENT Issue (determined by ACF taxonomy field 'published_date') that are in the featured category that ALSO have the ACF true/false field checked of 'slideshow_include'. Order should be set to 'rand'
 			I can handle the foreach loop part once query is set.
@@ -26,43 +26,43 @@
 			Will need to duplicate this query for the main archive issue pages as well. Difference is not most recent, but instead the current issue(term) you are viewing
 			-->
 			<ul class="slides">
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">		
-					<span class="photo-overlay"></span>		
+				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
+					<span class="photo-overlay"></span>
 					<div class="row"><div class="ten columns centered">
 						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
 						<a href="#">
-							<h1>Featured STory Title</h1>
-							<p>Here is some teaser Text</p>								
-						</a>					
+							<h1>Featured Story Title</h1>
+							<p>Here is some teaser Text</p>
+						</a>
 					</div>
 				</li>
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">		
-					<span class="photo-overlay"></span>		
+				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
+					<span class="photo-overlay"></span>
 					<div class="row"><div class="ten columns centered">
 						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
 						<a href="#">
-							<h1>Featured STory Title</h1>
-							<p>Here is some teaser Text</p>								
-						</a>					
+							<h1>Featured Story Title</h1>
+							<p>Here is some teaser Text</p>
+						</a>
 					</div>
 				</li>
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">		
-					<span class="photo-overlay"></span>		
+				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
+					<span class="photo-overlay"></span>
 					<div class="row"><div class="ten columns centered">
 						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
 						<a href="#">
-							<h1>Featured STory Title</h1>
-							<p>Here is some teaser Text</p>								
-						</a>					
+							<h1>Featured Story Title</h1>
+							<p>Here is some teaser Text</p>
+						</a>
 					</div>
-				</li>	
+				</li>
 			</ul>
 		</div><!-- /flexsldier -->
-		<div class="zbt-flex-nav"><a href="#" class="flex-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a><a href="#" class="flex-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>	
+		<div class="zbt-flex-nav"><a href="#" class="flex-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a><a href="#" class="flex-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
 	</section>
-	<?php 
-		$abouthead = get_field('heading', 'option'); 
-		$aboutcont = get_field('heading', 'option'); 
+	<?php
+		$abouthead = get_field('heading', 'option');
+		$aboutcont = get_field('heading', 'option');
 		if( $abouthead || $aboutcont) {
 	?>
 		<section class="issue-intro tcenter content-bar">
@@ -76,4 +76,3 @@
 	<?php } ?>
 <?php } // is front_page ?>
 
-	

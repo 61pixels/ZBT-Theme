c61da02580a8f4d72b1f0085430bfcc50a2ad42e
Kevin Hoffman <kevin@kevinwhoffman.com>
2016-08-05 00:55:17 -0400

    Replace static tiles with dynamic tiles.

--- front-page.php
+++ front-page.php
@@ -4,229 +4,54 @@
 
 	<?php get_template_part( 'template-parts/issue-head' ); ?>
 
+	<!-- Features -->
 	<div class="main-content-wrap zgreybg content-bar-l">
-	 <!-- this is the main query area, I'm thinking there are individual queries for each section since the last 4 categories have a totally different layout
-	 So a query from current issue for Featured category first, then new query for next etc.. If there is a better way I'm open -->
-	 	<?php zbt_get_latest_issue(); ?>
 		<a name="features"></a>
 		<section class="archive-grid-section master-cats">
 			<div class="row">
 				<div class="twelve columns tcenter">
 					<div class="cat-heading">
 						<div class="buffer">
-							<h2>Features</h2>
+							<h2><?= __( 'Features', 'zbt' ); ?></h2>
 						 </div>
 						 <a href="#top" class="backtop"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i></a>
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="six columns">
-					<article class="bshad grid-featured">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-			</div><!-- row -->
-			<div class="row">
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns last"> <!-- last item needs class of last added -->
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-			</div>
+			<?php zbt_tile_articles( 'features' ); ?>
 		</section>
 
-		<!-- Next Category, Alumni & Chapter News -->
+		<!-- Alumni & Chapter News -->
 		<a name="chapter"></a>
 		<section class="archive-grid-section master-cats">
 		   <div class="row">
 				<div class="twelve columns tcenter">
 					<div class="cat-heading ">
 						<div class="buffer">
-							<h2>Alumni &amp; Chapter News</h2>
+							<h2><?= __( 'Alumni &amp; Chapter News', 'zbt' ); ?></h2>
 						 </div>
 						 <a href="#top" class="backtop"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i></a>
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="six columns">
-					<article class="bshad grid-featured">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-				<div class="three columns">
-					<article class="bshad">
-						<figure class="grid-hover-item">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/images/large-test.jpg" alt="" class="grid-img-main" />
-							<a href="<?php the_permalink();?>" class="grid-mask-meta">
-								<span class="button binversed">More</span>
-							</a>
-						</figure>
-						<section class="post-grid-content">
-							<h1><a href="#">Feature article lorem ipsum dolor sit amet consectur</a></h1>
-							<a href="#" class="grid-more">Read Story</a>
-						</section>
-					</article>
-				</div>
-			</div><!-- row -->
+			<?php zbt_tile_articles( 'alumni-chapter-news' ); ?>
 		</section>
-		<!-- Foundation category removed just for simplicity sake for now, but that woudl go here next with same layout as above-->
+
+		<!-- Foundation -->
+		<a name="chapter"></a>
+		<section class="archive-grid-section master-cats">
+			<div class="row">
+				<div class="twelve columns tcenter">
+					<div class="cat-heading ">
+						<div class="buffer">
+							<h2><?= __( 'Foundation', 'zbt' ); ?></h2>
+						 </div>
+						 <a href="#top" class="backtop"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i></a>
+					</div>
+				</div>
+			</div>
+			<?php zbt_tile_articles( 'foundation' ); ?>
+		</section>
 
 		<!-- four column extra categorys with diff layout -->
 		<a name="legacy"></a>

a46f2231ba4ba60809f4553c6c0a3f9c1a6078c4
Kevin Hoffman <kevin@kevinwhoffman.com>
2016-08-08 13:06:37 -0400

    Make slideshow dynamic.

--- template-parts/issue-head.php
+++ template-parts/issue-head.php
@@ -18,45 +18,30 @@
 <?php if( is_front_page() || is_tax( 'issue_cats' ) ) { ?>
 	<section class="featured-hero">
 		<div class="flexslider">
-			<!--
-			obviously harcoded for now with some sample slides, need query here to output
-			Query in this homepage case needs to pull all posts from the MOST RECENT Issue (determined by ACF taxonomy field 'published_date') that are in the featured category that ALSO have the ACF true/false field checked of 'slideshow_include'. Order should be set to 'rand'
-			I can handle the foreach loop part once query is set.
-
-			Will need to duplicate this query for the main archive issue pages as well. Difference is not most recent, but instead the current issue(term) you are viewing
-			-->
-			<ul class="slides">
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
-					<span class="photo-overlay"></span>
-					<div class="row"><div class="ten columns centered">
-						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
-						<a href="#">
-							<h1>Featured Story Title</h1>
-							<p>Here is some teaser Text</p>
-						</a>
-					</div>
-				</li>
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
-					<span class="photo-overlay"></span>
-					<div class="row"><div class="ten columns centered">
-						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
-						<a href="#">
-							<h1>Featured Story Title</h1>
-							<p>Here is some teaser Text</p>
-						</a>
-					</div>
-				</li>
-				<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
-					<span class="photo-overlay"></span>
-					<div class="row"><div class="ten columns centered">
-						<div class="cat-heading no-line reversed"><div class="buffer"><h2>Issue Name</h2></div></div>
-						<a href="#">
-							<h1>Featured Story Title</h1>
-							<p>Here is some teaser Text</p>
-						</a>
-					</div>
-				</li>
-			</ul>
+			<?php
+			$issue = zbt_get_issue(); // Gets current issue being viewed.
+			$articles = zbt_get_articles( 'features', true ); // Features category, slideshow only.
+			if ( ! empty( $articles ) ) {
+				echo '<ul class="slides">';
+					foreach ( $articles as $post ) {
+						setup_postdata( $post );
+			?>
+						<li style="background-image:url('http://www.zbt.com/wp-content/uploads/2016/06/sample-photo2-600x338.jpg');">
+							<span class="photo-overlay"></span>
+							<div class="row"><div class="ten columns centered">
+								<div class="cat-heading no-line reversed"><div class="buffer"><h2><?php echo $issue->name ?></h2></div></div>
+								<a href="<?php the_permalink(); ?>">
+									<?php the_title('<h1>', '</h1>'); ?>
+									<p>Where does this teaser text come from?</p>
+								</a>
+							</div>
+						</li>
+			<?php
+					}
+					wp_reset_postdata();
+				echo '</ul>';
+			}
+			?>
 		</div><!-- /flexsldier -->
 		<div class="zbt-flex-nav"><a href="#" class="flex-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a><a href="#" class="flex-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
 	</section>

b40fac1cae324e0dd1917139ae0dc49e63ab16f7
Kevin Hoffman <kevin@kevinwhoffman.com>
2016-08-08 10:58:49 -0400

    Clean up trailing white space.

--- page-templates/template-isssues-archive.php
+++ page-templates/template-isssues-archive.php
@@ -5,24 +5,24 @@ Template Name: Issues Archive Grid
 ?>
 <?php get_header(); ?>
 
-<main role="main">		
+<main role="main">
 	<?php if(have_posts()) : ?>
 		<?php while(have_posts()) : the_post(); ?>
-		
+
 		<article class="main-content-wrap zgreybg">
-			<header class="art-intro tcenter zbluebg content-bar">				
+			<header class="art-intro tcenter zbluebg content-bar">
 				<div class="row">
-					<div class="ten columns centered">						
-						<h1>Issues Archive</h1>						
+					<div class="ten columns centered">
+						<h1>Issues Archive</h1>
 					</div>
 				</div>
 			</header>
-				
-			<section class="main-content-wrap zgreybg content-bar-l">		
-				<div class="row">				
+
+			<section class="main-content-wrap zgreybg content-bar-l">
+				<div class="row">
 					<div class="twelve columns">
-						<?php the_content(); ?>	
-						<?php 
+						<?php the_content(); ?>
+						<?php
 						// query goes here, needs to output all Issues in DESC order (newest first), sorted by ACF date field 'published_date' and only ones that have 'issue_status" ACF field = live
 						// Issues taxonomy is issue_cats
 						// needs to display cover photo, acf field 'cover_photo' and issue title, both clickable to issue archive page
@@ -63,9 +63,9 @@ Template Name: Issues Archive Grid
 					</div>
 				</div>
 			</section>
-		</article>				
+		</article>
 	<?php endwhile; ?>
-	<?php endif; ?>	
+	<?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
